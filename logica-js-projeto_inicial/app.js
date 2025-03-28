8655ee6fd400d38270a24ead6b6ebd386556f77c
Leonardo-RodriguesBR <leonardo.marketing@outlook.com.br>
2025-03-27 23:14:00 -0300

    Conflito do macaco doido

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -2,5 +2,5 @@ function contarCaracteres (caracteres){
     return caracteres.length;
 }
 
-let caracteres = 'Macaco Doido';
+let caracteres = 'Macaco Doido batalhando';
 console.log(contarCaracteres(caracteres));
