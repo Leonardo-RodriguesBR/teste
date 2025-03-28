191028fbf6afb783d1ac41b1ce9c851d8d95b7a8
Leonardo Correa <leonardo.marketing@outlook.com.br>
2025-03-27 23:28:10 -0300

    app.js

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -2,5 +2,5 @@ function contarCaracteres (caracteres){
     return caracteres.length;
 }
 
-let caracteres = 'Macaco Doido';
+let caracteres = 'Macaco Doido bugado';
 console.log(contarCaracteres(caracteres));
